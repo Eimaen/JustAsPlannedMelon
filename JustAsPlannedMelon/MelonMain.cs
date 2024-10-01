bd7f60b75c5f5415706158483c0d2f2d389b7239
Glitchtest51 <141680927+Glitchtest51@users.noreply.github.com>
2024-10-01 09:57:21 -0400

    make it more like the older version in one giant commit

---
+++ JustAsPlannedMelon/MelonMain.cs
@@ -0,0 +1,25 @@
+﻿using MelonLoader;
+using HarmonyLib;
+using Il2CppSteamworks;
+
+// Main
+namespace JustAsPlannedMelonFixed {
+    public class Core : MelonMod {
+        public override void OnInitializeMelon() {
+            LoggerInstance.Msg("Congratulations! You haven't paid for pixel Reimu.");
+        }
+    }
+}
+
+// Patch
+namespace JustAsPlannedMelon.Patch {
+    [HarmonyPatch(typeof(SteamApps), "GetEarliestPurchaseUnixTime", new Type[] { typeof(AppId_t) })]
+    public static class HkGetEarliestPurchaseUnixTime {
+        private static void Postfix(out uint __result) => __result = (uint)DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+    }
+
+    [HarmonyPatch(typeof(SteamApps), "BIsDlcInstalled", new Type[] { typeof(AppId_t) })]
+    public static class HkBIsDlcInstalled {
+        private static void Postfix(AppId_t appID, out bool __result) => __result = true;
+    }
+}
---
+++ JustAsPlannedMelon/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using MelonLoader;
+using System.Runtime.InteropServices;
+
+// MelonLoader Assembly Info
+[assembly: MelonInfo(typeof(JustAsPlannedMelonFixed.Core), "JustAsPlannedMelon", "2.0.0", "Eimaen/Glitchtest(51)")]
+[assembly: MelonGame("PeroPeroGames", "MuseDash")]
+
+// Setting ComVisible to False makes types in this assembly invisible
+// for COM components. If you need to access a type in this assembly via
+// COM, set the ComVisible attribute to TRUE for this type.
+[assembly: ComVisible(false)]
+
+// The following GUID serves to identify the type library if this project is to be visible to COM
+[assembly: Guid("8145A8E4-9C02-4A34-BE78-C6B00DA4A8D2")]
